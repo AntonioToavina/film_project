478ed7d341303a913604c9fe480de96a83a8de17
w417 <sandratraa67@gmail.com>
2023-03-21 15:41:34 +0300

    Ajout auteur dans la base de données et fichier de relai

--- src/sctructure.sql
+++ src/sctructure.sql
@@ -265,7 +265,26 @@ CREATE TABLE Limitpage(
 
 INSERT INTO limitpage(valeur) values(5);
 
-
+CREATE SEQUENCE personne_id_seq start 1 increment 1;
+create table personne(
+                         id integer primary key default nextval('personne_id_seq'),
+                         nom varchar(50),
+                         prenom varchar(50)
+);
+INSERT INTO personne(nom, prenom) VALUES
+                                      ('Doe', 'John'),
+                                      ('Smith', 'Jane'),
+                                      ('Williams', 'Mike'),
+                                      ('Brown', 'Sarah'),
+                                      ('Taylor', 'David'),
+                                      ('Johnson', 'Emily'),
+                                      ('Anderson', 'Chris'),
+                                      ('Martin', 'Amanda'),
+                                      ('Jones', 'Brian'),
+                                      ('Wilson', 'Laura');
+alter table scene add column auteur_id integer references personne(id);
+update scene set auteur_id = id where true;
+alter table scene alter column auteur_id set not null;
 
 -- Vue pour le status des scènes avec planifié ou non
 
